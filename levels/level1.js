208c19cebc577046a95fb8f444e5dd55a554d3c2
Daniel Lehmann <daniel.lehmann188@gmail.com>
2024-03-17 13:27:53 +0100

    added endboss

--- levels/level1.js
+++ levels/level1.js
@@ -44,5 +44,5 @@ const level1 = new Level(
 		new BackgroundObject('../assets/img/5_background/layers/3_third_layer/2.png', 719 * 5),
 		new BackgroundObject('../assets/img/5_background/layers/2_second_layer/2.png', 719 * 5),
 		new BackgroundObject('../assets/img/5_background/layers/1_first_layer/2.png', 719 * 5),
-	]
+	],
 );
