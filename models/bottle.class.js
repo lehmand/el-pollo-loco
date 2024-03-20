1c964aa2cbe283f0cc38b2c23dcfe93a4a6acdde
Daniel Lehmann <139554573+lehmand@users.noreply.github.com>
2024-03-20 11:39:26 +0000

    changed bottles to the ground

--- models/bottle.class.js
+++ models/bottle.class.js
@@ -10,13 +10,13 @@ class Bottle extends CollectableObject {
     constructor(y){
         super();
 
-        let bottleToShow = Math.round(Math.random() * 2)
+        let bottleToShow = Math.round(Math.random())
 
         this.loadImg(this.IMAGES[bottleToShow]);
 
 
         this.x = Math.round(Math.random() * 3000);
         this.y = y;
-        
+
     }
 }
