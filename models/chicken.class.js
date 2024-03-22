13fe361a22c77d714c26fb08d0b9b1123e101ff5
Daniel Lehmann <139554573+lehmand@users.noreply.github.com>
2024-03-22 11:31:32 +0000

    added all relevant images

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,39 +1,38 @@
 class Chicken extends MovableObject {
-    offsetY = 10;
-    y = 460 - this.height;
-    height = 120;
+  offsetY = 10;
+  y = 460 - this.height;
+  height = 120;
 
+  IMAGES_WALKING = [
+    '../assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+    '../assets/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+    '../assets/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png',
+  ];
 
-    IMAGES_WALKING = [
-        '../assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
-        '../assets/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
-        '../assets/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
-    ];
+  IMAGES_DEAD = [
+    '../assets/img/3_enemies_chicken/chicken_normal/2_dead/dead.png',
+  ];
 
-    IMAGES_DEAD = [
-        '../assets/img/3_enemies_chicken/chicken_normal/2_dead/dead.png',
-    ];
-    
-    constructor(){
-        super().loadImg('../assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
-        this.loadImages(this.IMAGES_WALKING);
+  constructor() {
+    super().loadImg(
+      '../assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png'
+    );
+    this.loadImages(this.IMAGES_WALKING);
 
-        this.x = 350 + Math.random() * 500;
-        this.speed = 0.15 + Math.random() * 0.25;
+    this.x = 350 + Math.random() * 500;
+    this.speed = 0.15 + Math.random() * 1.5;
 
-        this.animate();
-    }
+    this.animate();
+  }
 
-    animate(){
-        setInterval(() => {
-			this.moveLeft();
-            this.otherDirection = false;
-		}, 1000 / 60);
+  animate() {
+    setInterval(() => {
+      this.moveLeft();
+      this.otherDirection = false;
+    }, 1000 / 60);
 
-        
-        setInterval(() => {
-            this.playAnimation(this.IMAGES_WALKING);
-        }, 240)
-    }
-     
+    setInterval(() => {
+      this.playAnimation(this.IMAGES_WALKING);
+    }, 240);
+  }
 }
